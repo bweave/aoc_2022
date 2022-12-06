acd4a7b45ef1a3314667bbb1fc4d08180be68072
Brian Weaver <bdrums@gmail.com>
2022-12-06 08:13:49 -0500

    Syntax tree formatting for test_helper

--- test_helper.rb
+++ test_helper.rb
@@ -3,15 +3,15 @@ require "minitest/pride"
 
 class Minitest::Test
   def self.test(name, &block)
-    test_name = "test_#{name.gsub(/\s+/, '_')}".to_sym
+    test_name = "test_#{name.gsub(/\s+/, "_")}".to_sym
     defined = method_defined? test_name
     raise "#{test_name} is already defined in #{self}" if defined
     if block_given?
       define_method(test_name, &block)
     else
-      define_method(test_name) do
-        flunk "No implementation provided for #{name}"
-      end
+      define_method(
+        test_name
+      ) { flunk "No implementation provided for #{name}" }
     end
   end
 end
